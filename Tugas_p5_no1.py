ce957e3ab9a9d5d4f154611a044f55d018618e2e
XiaoMao15 <kagayaku15h@gmail.com>
2024-11-01 09:43:53 +0700

    Tugas5

---
+++ Tugas_p5_no1.py
@@ -0,0 +1,19 @@
+kendaraan = ['beat karbu','motor', 
+200,'pink',2]
+
+kendaraan.append('13jt') 
+kendaraan.append('matic') 
+print(kendaraan) 
+
+kendaraan.insert(2, 'honda') 
+print(kendaraan)
+
+print(type (kendaraan)) 
+print(type (kendaraan[0])) 
+print(type (kendaraan[1])) 
+print(type (kendaraan[2])) 
+print(type (kendaraan[3])) 
+print(type (kendaraan[4])) 
+print(type (kendaraan[5])) 
+print(type (kendaraan[6])) 
+print(type(kendaraan[7]))
